12113a042a368942eab36e1a60ab9dae806dc267
felipe <profissionalf.anre@gmail.com>
2021-01-26 10:55:28 -0300

    feat: add first module in lesson

---
+++ my-app/src/app/myfirst.component.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+
+
+@Component({
+  // selector: '.my-first',
+  selector: 'my-first',
+  // selector: '[my-first]',
+  templateUrl: './myfirst.component.html',
+  // styles: [`
+  //   .h2 {
+  //     text-align: center;
+  //     font-size: 20px
+  //   }
+  // `]
+  styleUrls: ['./myfirst.component.css']
+})
+
+export class MyFirstComponent {
+
+}
